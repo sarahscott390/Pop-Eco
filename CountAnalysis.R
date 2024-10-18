9c674b6336b479e4592b3a33559bd1decc6b6acf
sarahscott390 <sarahscott390@u.boisestate.edu>
2024-10-18 17:02:52 -0600

    SS edit

---
+++ CountAnalysis.R
@@ -0,0 +1,241 @@
+#######################################################################
+#######################################################################
+##     This script was created by Dr. Jen Cruz as part of            ##
+##            the Applied Population Ecology Class                  ###
+##                                                                   ##  
+## Here we import our cleaned data for a single year of point count  ##
+#  observations for Piute ground squirrels at the NCA and run a      ##
+## closed population N-mixture analysis. The model is hierarchical    #
+#  with : (1) an ecological submodel linking abundance to             #
+## environmental predictors at each site; (2) an observation submodel #
+## linking detection probability to relevant predictors.             ##
+##                                                                   ##
+# Abundance is expected to be higher in sites with more sagebrush     #
+# and lower in those with more cheatgrass.                            #                                        #
+# Detection may be related to observer effects and to time of day     #
+#######################################################################
+
+##### Set up your workspace and load relevant packages -----------
+# Clean your workspace to reset your R environment. #
+rm( list = ls() )
+# Check that you are in the right project folder
+getwd()
+
+#install relevant packages
+install.packages( 'Rtools' )
+install.packages( "nmixgof" ) #for evaluating N-mixture models
+#load relevant packages
+library( tidyverse )#includes dplyr, tidyr and ggplot2
+library( unmarked ) #runs N-mixture models
+library( MuMIn ) #calculates pseudo-R^2
+library( AICcmodavg) #gof tests (Duarte et al. 2018)
+library( nmixgof ) #more gof tests (Knape et al. 2018)
+## end of package load ###############
+###################################################################
+#### Load or create data -----------------------------------------
+# set directory where your data are:
+datadir <- paste( getwd(), "/Data/", sep = "" )
+# load cleaned data
+closeddf <- read.csv( file = paste( datadir, "closed_counts.csv", sep = ""),
+                      header = TRUE )
+#view
+head( closeddf ); dim( closeddf ) 
+#### End of data load -------------
+####################################################################
+##### Ready data for analysis --------------
+# Let's define our unmarked dataframe:
+# Start by defining which columns represent the response (counts):
+umf <- unmarkedFramePCount( 
+  y = as.matrix( closeddf[ ,c("count.j1", "count.j2", "count.j3")]),
+  # Define predictors at the site level:
+  siteCovs = closeddf[ ,c("sagebrush", "cheatgrass")],
+  # Define predictors at the survey level as a list:
+  obsCovs = list( obsv = closeddf[ ,c("observer.j1", "observer.j2", "observer.j3")],
+                  time = closeddf[ ,c('time.j1', 'time.j2', 'time.j3') ],
+                  time2 = cbind( (closeddf$time.j1)^2, (closeddf$time.j2)^2, 
+                                 (closeddf$time.j3)^2 ) ) ) 
+
+# View summary of unmarked dataframe:
+summary( umf )
+#now scale ecological predictors:
+sc <- apply( siteCovs(umf), MARGIN = 2, FUN = scale )
+# We replace the predictors in our unmarked dataframe with the scaled values:
+siteCovs( umf ) <- sc
+#now for time:
+timesc <- as.vector(scale( obsCovs(umf)[2] ))
+#replace with scaled values:
+obsCovs(umf)[2] <- timesc
+time2sc <- as.vector(scale( obsCovs(umf)[3] ))
+#replace with scaled values:
+obsCovs(umf)[3] <- time2sc
+
+#recheck
+summary( umf )
+### end data prep -----------
+### Analyze data ------------------------------------------
+# We are now ready to perform our analysis. We start with a full model:
+fm.closed <- pcount( ~ 1 + obsv + time + time2
+                     ~ 1 + sagebrush + cheatgrass, 
+                     #Define the maximum possible abundance
+                     #during the primary occasion
+                     K = 40,
+                     data = umf )
+# Note that we start with the observation submodel #
+#We then define the ecological submodel 
+# You should try alternative K values to make sure that your model 
+#isn't sensitive to the value you provided. Note: The higher it is, the longer it will take the model to run.
+
+# View model results:
+fm.closed
+
+# Estimate confidence intervals:
+confint( fm.closed, type = "state" )
+# coefficients for detection submodel:
+confint( fm.closed, type = 'det' )
+#
+### 1. Based on the overlap of the 95% CIs for your predictor coefficients, #
+# which may be important to each of your responses? #
+# Answer: 
+# Abundance: Sagebrush's CI does not overlap zero so it is likely important, while cheatgrass does overlap zero and is likely not important.
+# Detection: All observer CIs overlap zero, so it seems that they are not important, while Time's CI does not overlap zero and is important.
+#
+# Time^2 explanation from online references/chat for general understanding: The CI for time (0.732 to 1.788) does not overlap zero, indicating a significant positive effect on detection probability as time increases. Conversely, the CI for time² (-1.986 to -0.887) is negative and does not overlap zero, suggesting a quadratic relationship where detection increases with time to a point but may decline afterward.
+#
+#What is the mean abundance from our model?  12.06
+exp(coef(fm.closed[1]))
+#############end full model ###########
+##########################################################################
+# Model fit and evaluation -----------------------------------------------
+# We start with goodness of fit (GoF) outlined by Duarte et al. 2018 #
+# Ecological modelling 374:51-59 and available via AICmodavg package #
+# The Nmix.gof.test relies on a Pearson chi-square to assess the fit of #
+# N-mixture models. The approach uses bootstrapping to estimate the p values #
+# The test also estimates a c-hat measure of overdispersion, as the  #
+# observed test statistic divided by the mean of the simulated test statistics #
+
+# Let's compute observed chi-square, assess significance, and estimate c-hat
+gof.boot <- Nmix.gof.test( fm.closed, nsim = 100, 
+                           print.table = TRUE )
+#view
+gof.boot
+### 2. What does the output tell us about our model fit?
+# Answer:  Chi-square = 117.2983.  P value = .86 (seems fine?)... no evidence there that the model is a poor fit.  C-hat = .83, indicating some underdispersion (c-hat < 1 = underdispersed, c-hat > 1 = overdispersed).  Seems like overall, the model fits okay.
+#
+#
+#
+# We also evaluate how well our full model did against the null model # 
+# by estimating pseudo-R^2, based on Nagelkerke, N.J.D. (2004) A Note #
+# on a General Definition of the Coefficient of Determination. Biometrika 78,#
+# pp. 691-692.#
+# We run the null model
+fm.null <- pcount( ~ 1 ~ 1,
+                   K = 40, data = umf )
+#view
+fm.null
+# Now build the list with the two models of interest:
+rms <- fitList( 'full' = fm.closed,
+                'null' = fm.null )
+# Then use model selection function from unmarked, defining which is the null:
+unmarked::modSel(rms, nullmod = "null" )
+### 3. What does this tell us?
+# Answer:  It tells us that the full model fits the data better than the null model.  AIC is lower and speudo-Rsquared is high.  Habitat and survey predictors are important to abundance and detection.  
+#
+# Now use gof checks outlined in Knape et al. 2018 MEE 9:2102-2114
+# We start by estimating overdispersion metrics 
+chat( fm.closed, type = 'marginal' )
+chat( fm.closed, type = 'site-sum' )
+chat( fm.closed, type = 'observation' )
+
+# Plot rq residuals against untransformed numeric predictors. This may help
+# detect problems with the functional form assumed in a model
+residcov( fm.closed )
+### 4. What do the plots tell you?
+# Answer:  marginal c-hat = 0.83, site-sum c-hat = 0.88, and observation c-hat = 0.87. All these values are below 1, indicating a bit of underdispersion in the model.   Residual plot looks solid; residual data points are more or less evenly scattered with no curves or major clustering, outliers are not dramatic.
+#
+# Plot residuals against fitted values. Site-sum randomized quantile residuals
+# are used for site covariates while marginal residuals are used for
+# observation covariates. 
+residfit( fm.closed, type = 'site-sum' )
+# Plot the observation model residuals
+residfit( fm.closed, type = 'observation' )
+### 5. What did Knape et al. 2018 say these residuals were useful for?
+# Answer: (pg. 2105) Site-sum residuals: "The idea of aggregating counts across sites is to make residuals independent and potentially to increase their informativeness in cases where counts are sparse. Since there is one site-sum residual per site, they can be used in plots against site-specific covariates."  Observation residuals: " The idea is to compute residuals from the observation model only by conditioning on the abundances, with the intent of more specifically checking the detection part of the model."
+#
+# Now plot Qq plots of randomized residuals against standard normal quantiles. #
+# Under a good fit, residuals should be close to the identity line. 
+residqq( fm.closed, type = 'site-sum' )
+residqq( fm.closed, type = 'observation' )
+### 6. What do these plots indicate? 
+# Answer:  Both site sum and observation residuals stay near or on the line (no significant departures from the line), it seems like they are pretty normally distributed/the model fit is solid
+# 
+### 7. What is some of the advice recommended by Knape et al. 2018 if we want 
+# to use abundance estimates from these N-mixture models?
+# Answer:  To avoid overestimates of abundance when using N-mixture models, Knape et al suggest using negative binomial, zero inflated Poisson or adding random effects.
+#
+### 8. Now try fitting other functional forms (e.g. ZIP or Negative Binomial)
+# Do you get a better fit?
+# Answer: The negative binomial (below) has a slightly higher AIC than the previous model (651 vs 649) so it is not performing better than the N-mixture model we previously fit.  
+#
+# # Negative Binomial model
+fm.nb <- pcount(~ 1 + obsv + time + time2 ~ 1 + sagebrush + cheatgrass, 
+                mixture = "NB", 
+                K = 40, 
+                data = umf)
+
+summary(fm.nb)
+
+# AIC
+aic_nb <- AIC(fm.nb)
+
+#
+### 9. Are there other ways to fit time that may make more sense?
+# Answer:  We could treat time as a factor/categorical variable?
+#
+
+#########################################################################
+##### Summarizing model output ##############
+# Estimate partial prediction plots for predictors with 95% CIs not overlapping zero:
+# Start by creating our datasets to predict over
+# how many values do we use:
+n <- 100
+# what are the min max times:
+closeddf %>% select( time.j1, time.j2, time.j3 ) %>% 
+  summarise_all(list(min, max) )
+#use them to define your bounds:
+Time <- round(seq( 0, 360, length.out = n ),0)
+Time2 <- Time^2
+time.std <- scale( Time )
+time2.std <- scale( Time2 )
+# now for detection
+detData <- data.frame( obsv = factor(c("tech.1", "tech.1","tech.1", "tech.1"), 
+                                     levels = c("tech.1", "tech.2","tech.3", "tech.4") ), 
+                       time = time.std, time2 = time2.std )
+#predict partial relationship:
+pred.time <- predict( fm.closed, type = "det", newdata = detData, 
+                      appendData = TRUE )
+
+# create plot for detection submodel:
+timep <- cbind( pred.time[,c("Predicted", "lower", "upper") ], Time ) %>%
+  # define x and y values
+  ggplot(., aes( x = Time, y = Predicted ) ) + 
+  #choose preset look
+  theme_bw( base_size = 15 ) +
+  # add labels
+  labs( x = "Time (mins pass 6:00am)", y = "Probability of Occupancy" ) +
+  # add band of confidence intervals
+  geom_smooth( aes(ymin = lower, ymax = upper ), 
+               stat = "identity",
+               size = 1.5, alpha = 0.5, color = "grey" ) +
+  # add mean line on top
+  geom_line( size = 2 ) 
+#view
+timep
+
+############################################################################
+################## Save your data and workspace ###################
+# Save workspace:
+save.image( "CountResults.RData" )
+
+########## End of saving section ##################################
+
+############# END OF SCRIPT #####################################
---
+++ CountDataPrep.R
@@ -0,0 +1,172 @@
+#######################################################################
+#######################################################################
+####   This script was created by Dr. Jen Cruz as part of      ########
+####          the Applied Population Ecology Class              #######
+####                                                           ########
+# Our study species is the Piute ground squirrel, Spermophilus        # 
+# mollis at the Morley Nelson Birds of Prey Nature Conservation Area. #
+# Their abundance is influenced by drought, low temperatures when     #
+# they emerge from hibernation in Feb and high temperatures in        #
+# in April-May                                                        #
+# 50 sites were randomly selected for repeated count surveys over     #
+# three days. Surveys were repeated over multiple years ()            #
+# Surveys involved point counts for 2min where all individuals detected #
+# over a 200 m radius were recorded.                                  # 
+#######################################################################
+##### Set up your workspace and load relevant packages -----------
+# Clean your workspace to reset your R environment. #
+rm( list = ls() )
+# Check that you are in the right project folder
+getwd()
+
+# load packages:
+library( tidyverse ) 
+## end of package load ###############
+###################################################################
+#### Load or create data -----------------------------------------
+# set directory where your data are. 
+# I have it in a Data folder in my Rstudio project:
+datadir <- paste( getwd(), "/Data/", sep = "" )
+
+# load observed occurrences:
+obs_df <- read.csv( file = paste( datadir, "obs_df.csv", sep = ""),
+                    header = TRUE )
+
+#view
+head( obs_df ); dim(obs_df)
+#load predictor data
+preddf <- read.csv( file = paste( datadir, "predictors.csv", sep = ""),
+                    header = TRUE )
+#view
+head( preddf ); dim( preddf )
+########## end of data load ################################
+#######################################################################
+######## explore data #############
+# Start viewing our observations dataframe:
+head( obs_df ); dim( obs_df )
+# Our observations dataframe metadata:
+# o.sites = site id (I)
+# counted = was the site also surveyed using point counts
+# marked = was the site also trapped
+# year = year of sampling (primary seasons) T
+# pres.j1, pres.j2, pres.j3: observation of occurrence for j surveys 1:3
+# count.j1, count.j2, count.j3 = point counts for j surveys 1:3
+# observer.j1, observer.j2, observer.j3 = observer IDs for each j survey, site
+# time.j1, time.j2, time.j3 = time of day that the point count were conducted #
+# where 0 is 0 minutes pass 6am to 360 minutes at noon.
+
+# We start with our robust design data ##########
+# Remember that not all sites were surveyed with point counts but only # 
+# those were 'counted' column = yes. We start by filtering the dataframe #
+# to sites that were counted. We also remove presence/absence records. #
+# The same observers that conducted the presence surveys, conducted #
+# the counts so those columns remain. Note that time of day can now be #
+# included as they reflect the time that the point counts were conducted #
+
+# Also note that our siteXyear predictor data is the same as what we #
+# used in the occupancy analyses. We therefore do not explore those for #
+# correlation, outliers, spread, since we have already done it. # 
+# Refer to OccDataPrep.R for details of how to do that if you are #
+# working with your own data. #
+
+head( obs_df )
+opendf <-  obs_df %>% 
+  # keep only rows where sites had point counts:
+  dplyr::filter( counted == "yes" ) %>%
+  #select columns to keep:
+  dplyr::select( o.sites, counted, year, count.j1, count.j2, count.j3,
+                 observer.j1, observer.j2, observer.j3,
+                 time.j1, time.j2, time.j3 ) 
+#check
+head( opendf ); dim( opendf )
+
+# We append predictors:
+opendf <- preddf %>% 
+  dplyr::select( o.sites, year, cheatgrass, sagebrush, Feb.minT, AprMay.maxT ) %>%
+  right_join( opendf, by = c("o.sites", "year") )
+#check
+head( opendf ); dim( opendf )
+
+### 1. Why do we use right_join instead of left_join or other?
+# Answer: We want to retain all records from preddf in the new dataset.  If we used left join, we would have potentially lost   predictor data for sites that weren't surveyed.
+#
+# opendf contains information about sites where point counts were conducted.
+# preddf contains predictor data for all sites and years.
+#
+
+# Let's define some parameters for our robust design:
+# How many sites were sampled?
+I <- length( unique( opendf$o.sites ) )
+# What years were sampled (primary seasons)?
+yrrange <- sort( unique( opendf$year) )
+#How many years (i.e., primary seasons )?
+T <- length( yrrange )
+# How many replicate surveys
+J <- 3
+#view
+I; yrrange; T; J
+
+###########
+
+# We now need to decide which year to use for our closed analysis ###
+# let's work out what our counts look like each year:
+# we use our opendf and group by year by site:
+opendf %>% group_by( year, o.sites ) %>%
+  # we calculate the maximum count observed during the primary season
+  # this is our 'naive' estimate of abundance for that site that year:
+  # why?
+  mutate( maxc = max( count.j1, count.j2, count.j3 ) ) %>%
+  # we select columns we need for plotting
+  dplyr::select( o.sites, year, maxc ) %>% ungroup() %>%
+  # we group data by year
+  ggplot(., aes(group = year )) +
+  # choose a preset theme
+  theme_bw( base_size = 15 ) + 
+  # plot histogram of abundances: 
+  geom_histogram( aes( maxc ), binwidth = 10 ) +
+  # make a different plot for each year:
+  facet_wrap( ~year, ncol = 3, scales = "free" )
+# X-axis: maximum count of Piute ground squirrels observed at each site during the primary season for each year. This value is calculated as the highest count from the three replicate surveys for each site in a given year.
+# Y-axis: frequency (or count) of sites that fall into each bin of the maximum count. Essentially, it shows how # many sites had a particular maximum count of squirrels.
+#
+#how are counts changing at each site
+opendf %>% group_by( year, o.sites ) %>%
+  mutate( maxc = max( count.j1, count.j2, count.j3 ) ) %>%
+  dplyr::select( o.sites, year, maxc ) %>% ungroup() %>%
+  # we group data by year
+  ggplot(., aes(color = as.factor(o.sites), x = year, y = maxc )) +
+  theme_bw( base_size = 15 ) +
+  geom_line()
+
+### 2. What do we see? What is happening to site abundances, but also to #
+# site occupancy? 
+# Answers: There seem to have been fewer high maximum counts (ex. less counts of ~20) in certain years; 2010, 2011, 2012, 2013, 2014, 2016, 2017, and 2018.  
+# Counts seem somewhat consistent overall though, based on the second graphic.  Some sites saw a jump and a few trend down but not steeply.  This takeaway seems inconsistent with our discussions thus far so I'm unsure if I am interpreting things correctly.
+
+# We need to choose a year for our single season (closed population) analysis.
+# Based on the plots I will choose 2008. I think later years may require #
+# a zero-inflated Poisson model to account for all the empty sites #
+
+# We create our closeddf:
+closeddf <- opendf %>% 
+  #filter only rows for desired year *choose a different year than JC*:
+  dplyr::filter( year == 2012 ) 
+
+################################################################
+##########    save relevant data and workspaces     ###########
+#save open dataframe in our data folder. Make sure not to overwrite #
+# your occupancy files:
+write.csv( opendf, paste( getwd(),"/Data/open_counts.csv", sep = "" ),  
+           row.names = FALSE )
+
+#save closed dataframe in our data folder:
+write.csv( closeddf, paste( getwd(),"/Data/closed_counts.csv", sep = "" ),  
+           row.names = FALSE )
+
+# if you want to save your workspace, because you are still #
+# working through it use the following command:
+save.image( "CountPrepWorkspace.RData" )
+########## End of saving section ##################################
+################## Save your data and workspace ###################
+
+############# END OF SCRIPT ########################################
